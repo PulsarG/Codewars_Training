79f813e6c8dacd587609aecf51c68eef9fee6fa9
PulsarG <pulsar.g.projects@gmail.com>
2024-02-13 18:26:29 +0500

    test3#1

---
+++ ShortestWord.go
@@ -0,0 +1,28 @@
+// Shortest Word
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	longString := "dfg dfgg dfggg sf"
+
+	words := strings.Fields(longString)
+
+	length := 1
+
+	for i, word := range words {
+		if i == 0 {
+			length = len(word)
+		}
+
+		if length > len(word) {
+			length = len(word)
+		}
+	}
+
+	fmt.Println(length)
+}
