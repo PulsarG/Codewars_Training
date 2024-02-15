f7a5581988171603a01cd8ea2fad157677bcdb54
PulsarG <pulsar.g.projects@gmail.com>
2024-02-15 20:03:27 +0500

    test#8.2+

--- IsaNumberPrime.go
+++ IsaNumberPrime.go
@@ -1,19 +1,27 @@
 // Is a number prime? - 6 kyu
-// test#8.1
+// test#8.2+ready
 
 package main
 
 import (
 	"fmt"
+	"math"
 	//"strings"
 )
 
 func main() {
-	n := 7
+	n := 8438644364383
+	if n == 2 || n == 3 || n == 5 || n == 7 { fmt.Println(true) }
+  
+if n <= 1 { fmt.Println(false) }
 
-	for i := 2; i < n; i++ {
+maxDivisor := int(math.Sqrt(float64(n))) + 1 
+  
+for i := 2; i <= maxDivisor; i++ {
+  if i > 3 && i%2 == 0 {continue}
+  if i > 3 && i%3 == 0 {continue}
 		if (n % i) == 0 {
-			fmt.Println(i, false)
+			fmt.Println(false)
 		}
 	}
 
