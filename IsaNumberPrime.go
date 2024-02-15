0336d2fde8439608806987022126dcf90aec4c9f
PulsarG <pulsar.g.projects@gmail.com>
2024-02-15 20:04:02 +0500

    test#8.2+ready

--- IsaNumberPrime.go
+++ IsaNumberPrime.go
@@ -27,3 +27,18 @@ for i := 2; i <= maxDivisor; i++ {
 
 	fmt.Println(true)
 }
+
+
+// !! ANSWER 
+
+/* func IsPrime(n int) bool {
+	if n < 2 {
+	  return false
+	}
+	for i:=2; i<=int(math.Sqrt(float64(n))); i++ {
+	  if n%i == 0 {
+		return false
+	  }
+	}
+	return true
+  } */
