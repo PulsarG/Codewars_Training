2d9110796e7bddcf537cf4e6763b5a061a199a36
PulsarG <pulsar.g.projects@gmail.com>
2024-02-16 19:57:13 +0500

    test#11.2+ready

--- strings/NotVerySecure.go
+++ strings/NotVerySecure.go
@@ -1,5 +1,5 @@
 // Not very secure - 5 kyu
-// test#11.1
+// test#11.2+ready
 
 package main
 
@@ -18,3 +18,11 @@ func main() {
 	fmt.Println(IsLetter("123"))
 	fmt.Println(IsLetter("рои134432-=-!№;;"))
 }
+
+
+// !! BEST ANSWER 
+
+/* func alphanumeric(s string) bool {
+	r := regexp.MustCompile("^[a-zA-Z0-9]+$")
+	return r.MatchString(s)
+} */
