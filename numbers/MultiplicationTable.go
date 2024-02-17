108d8ee618a57384ab1cd0d9352296815528fa92
PulsarG <pulsar.g.projects@gmail.com>
2024-02-17 16:39:30 +0500

    test#11.3+ready

--- numbers/MultiplicationTable.go
+++ numbers/MultiplicationTable.go
@@ -1,5 +1,5 @@
 // Multiplication table - 6 kyu
-// test#11.2
+// test#11.3+ready
 
 package main
 
@@ -12,15 +12,10 @@ import (
 
 func main() {
 	startNumber := 3
-
 	var resultArr [][]int
-
 	for i := 1; i <= startNumber; i++ {
-
 		resultArr = append(resultArr, createNArr(i, startNumber))
-
 	}
-
 	fmt.Println(resultArr)
 }
 
@@ -31,3 +26,16 @@ func createNArr(multyplitRate, startNumber int) []int {
 	}
 	return workArr
 }
+
+
+// !! BEST ANSWER 
+
+/* func MultiplicationTable(size int) [][]int {
+	res := make([][]int, size)
+	for i := 0 ; i < size ; i ++ {
+	  for x := 1 ; x < size + 1 ; x ++ {
+		res[i] = append(res[i], (i + 1) * x)
+		}}
+	return res
+		
+  } */
