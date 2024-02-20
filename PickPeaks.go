ee3868ac44d3c889ab63c1bd0760d1cd8500c2eb
PulsarG <pulsar.g.projects@gmail.com>
2024-02-20 14:53:29 +0500

    test#15.1

---
+++ PickPeaks.go
@@ -0,0 +1,29 @@
+// Pick peaks - 5 kyu
+// test#15.1
+
+package main
+
+import (
+	"fmt"
+)
+
+var startArr = []int{1, 2, 3, 6, 4, 1, 2, 3, 2, 1}
+
+type PosPeaks struct {
+	Pos   []int
+	Peaks []int
+}
+//	PosPeaks{Pos: []int{3, 7}, Peaks: []int{6, 3}}, */
+
+func main() {
+
+	// ** Перебор массива в поиске пиков
+	// ** Поиск пиков, кроме первого и последнего
+	// ** Поиск плато
+	// ** Запись пиков и плато
+
+	// ** Запись позиций пиков по массиву пиков
+
+}
+
+// !! BEST ANSWER
