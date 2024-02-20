75247516c3f522f9bc820a5bff74572a8c25a07c
PulsarG <pulsar.g.projects@gmail.com>
2024-02-20 15:49:19 +0500

    test#15.3+

--- PickPeaks.go
+++ PickPeaks.go
@@ -1,5 +1,5 @@
 // Pick peaks - 5 kyu
-// test#15.2
+// test#15.3+
 
 package main
 
@@ -7,34 +7,52 @@ import (
 	"fmt"
 )
 
-var startArr = []int{1, 2, 3, 6, 4, 1, 2, 3, 2, 1}
+var startArr = []int{3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1}
 
 type PosPeaks struct {
 	Pos   []int
 	Peaks []int
 }
 
-//	PosPeaks{Pos: []int{3, 7}, Peaks: []int{6, 3}}, */
+//	PosPeaks{Pos: []int{3, 7, 10}, Peaks: []int{6, 3, 2}}
 
 func main() {
 	p := &PosPeaks{}
-	
+
 	fmt.Println(searchPicks(startArr, p))
 	// ** Поиск плато
 	// ** Запись пиков и плато
 }
 
 func searchPicks(startArr []int, p *PosPeaks) PosPeaks {
-	for i, num := range startArr {
+	for i := 1; i < len(startArr); i++ {
+		num := startArr[i]
+		a := i
 		if i == 0 || i == (len(startArr)-1) {
 			continue
 		}
 
-		if num > startArr[i-1] && num > startArr[i+1] {
+		isPeack := (num > startArr[i-1] && num > startArr[i+1])
+		isPlatoPeak := false
+
+		if num == startArr[i+1] {
+
+			for i += 2; i < len(startArr); i++ {
+				if num == startArr[i] {
+					continue
+				} else if num > startArr[i] {
+					isPlatoPeak = true
+					break
+				}
+			} // end for
+		} // end if
+
+		if isPeack || isPlatoPeak {
 			p.Peaks = append(p.Peaks, num)
-			p.Pos = append(p.Pos, i)
-		}
-	}
+			p.Pos = append(p.Pos, a)
+		} // end if
+
+	} // end for
 
 	return *p
 }
