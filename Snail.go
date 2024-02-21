61717028f61618a90d126229d8cbd9c5f1ab3431
PulsarG <pulsar.g.projects@gmail.com>
2024-02-21 22:48:17 +0500

    test#16.2

--- Snail.go
+++ Snail.go
@@ -1,6 +1,6 @@
 // Snail - 4 kyu
 // https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/go
-// test#16.1
+// test#16.2
 
 package main
 
@@ -10,13 +10,19 @@ import (
 
 func main() {
 	startMap := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-	// res = []int{1, 2, 3, 6, 9, 8, 7, 4, 5}
+	res := []int{1, 2, 3, 6, 9, 8, 7, 4, 5}
 	result := []int{}
+	isWork := true
 
-	result = append(result, startMap[0][0])
+	for i := 0; i < len(startMap[0]); i++ {
+		result = append(result, startMap[0][i])
+	}
+
+	//startMap = append(startMap[:0], startMap[0+1:]...)
+	/* result = append(result, startMap[0][0])
 	result = append(result, startMap[0][1])
 	result = append(result, startMap[0][2])
-
+	*/
 	result = append(result, startMap[1][2])
 
 	result = append(result, startMap[2][2])
@@ -26,14 +32,12 @@ func main() {
 	result = append(result, startMap[1][0])
 	result = append(result, startMap[1][1])
 
-	// 0- 0 1 2
-	// 1- 2
-	// 2- 2 1 0
-	// 1- 0 1
+	startMap = append(startMap[:0], startMap[0+1:]...)
 
-	fmt.Println(result)
-}
-
-func Snail(snaipMap [][]int) []int {
-	return []int{}
+	for i, num := range result {
+		if num != res[i] {
+			isWork = false
+		}
+	}
+	fmt.Println(isWork, startMap)
 }
