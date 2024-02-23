35caf9749facf2d1f15c4e08ceb0be5a0460c507
PulsarG <pulsar.g.projects@gmail.com>
2024-02-24 01:54:21 +0500

    test#17.1

---
+++ SoManyPermutations.go
@@ -0,0 +1,22 @@
+// So Many Permutations - 4 kyu
+// https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/go
+// test#17.1
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+}
+
+/* With input 'a':
+Your function should return: ['a']
+
+With input 'ab':
+Your function should return ['ab', 'ba']
+
+With input 'abc':
+Your function should return ['abc','acb','bac','bca','cab','cba'] */
