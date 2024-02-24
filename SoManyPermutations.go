922bd6d38df173b07b26fd1ad2cd37fbf87300e1
PulsarG <pulsar.g.projects@gmail.com>
2024-02-24 18:31:39 +0500

    test#17.2

--- SoManyPermutations.go
+++ SoManyPermutations.go
@@ -1,16 +1,37 @@
 // So Many Permutations - 4 kyu
 // https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/go
-// test#17.1
+// test#17.2
 
 package main
 
 import (
 	"fmt"
+	//"strings"
 )
 
 func main() {
+	startStr := "abc"
+	var arrLiteral []string
 
+	for _, str := range startStr {
+		arrLiteral = append(arrLiteral, string(str))
+	}
+
+
+	fmt.Println(arrLiteral)
 }
+
+В цикле индекс = длина - 1
+	индекс литерал - первый в новой строке
+	запуск метода перебора всех литералов, кроме индекса
+		внутри функции рекурсия - создается массив без индекса и цикл со старта
+
+//generateCombinations("", n)
+
+// Разбиение на руны
+// Алгорит перебор
+// сбор в строки
+// Добавление в массив
 
 /* With input 'a':
 Your function should return: ['a']
