9e2edf6962549c5c803dce3e53475d3c3417ecdc
PulsarG <pulsar.g.projects@gmail.com>
2024-02-28 20:20:20 +0500

    test#18.4

--- TheMillionthFibonacciKata.go
+++ TheMillionthFibonacciKata.go
@@ -1,6 +1,6 @@
 // The Millionth Fibonacci Kata - 3 kyu
 // https://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/go
-// test#18.3+
+// test#18.4
 
 package main
 
@@ -9,6 +9,10 @@ import (
 	"math/big"
 	//"strings"
 )
+
+// !! Отрицательные
+// ** F(n) = F(n+2) - F(n+1)
+// ** F(-n) = (-1)**(n+1)*F(n)
 
 // !! Использовать лишь три индекса для экономии памяти
 
@@ -25,12 +29,10 @@ func main() {
 			fibArr[1] = *big.NewInt(int64(i))
 			fibArr[2] = *big.NewInt(int64(i))
 		} else {
-
 			a := fibArr[0]
 			b := fibArr[1]
-			c := new(big.Int).Add(&a, &b)
-			fibArr[2] = *c
-
+			//c := new(big.Int).Add(&a, &b)
+			fibArr[2] = *new(big.Int).Add(&a, &b)
 			fibArr[0] = fibArr[1]
 			fibArr[1] = fibArr[2]
 		}
