8a828c5cdd1131d5e18c02a006837274bb4e5191
PulsarG <pulsar.g.projects@gmail.com>
2024-02-29 20:47:51 +0500

    test#18.7

--- TheMillionthFibonacciKata.go
+++ TheMillionthFibonacciKata.go
@@ -1,6 +1,6 @@
 // The Millionth Fibonacci Kata - 3 kyu
 // https://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/go
-// test#18.6
+// test#18.7
 
 package main
 
@@ -17,7 +17,7 @@ import (
 // !! Использовать лишь три индекса для экономии памяти
 
 func main() {
-	n := 100
+	n := -10
 	var fibArr [3]big.Int
 
 	if n >= 0 {
@@ -30,28 +30,18 @@ func main() {
 				fibArr[1] = *big.NewInt(int64(i))
 				fibArr[2] = *big.NewInt(int64(i))
 			} else {
-				a := fibArr[0]
-				b := fibArr[1]
-
-				fibArr[2] = *new(big.Int).Add(&a, &b)
+				fibArr[2] = *new(big.Int).Add(&fibArr[0], &fibArr[1])
 				fibArr[0] = fibArr[1]
 				fibArr[1] = fibArr[2]
 			}
 		}
 	} else {
-		for i := 0; i >= int(n); i-- {
-			if i == 0 {
-				fibArr[0] = *big.NewInt(int64(i))
-				fibArr[1] = *big.NewInt(int64(i))
-				fibArr[2] = *big.NewInt(int64(i))
-			} else if i == -1 {
+		for i := -1; i >= int(n); i-- {
+			if i == -1 {
 				fibArr[1] = *big.NewInt(int64(i))
 				fibArr[2] = *big.NewInt(int64(i))
 			} else {
-				a := fibArr[0]
-				b := fibArr[1]
-
-				fibArr[2] = *new(big.Int).Sub(&a, &b)
+				fibArr[2] = *new(big.Int).Sub(&fibArr[0], &fibArr[1])
 				fibArr[0] = fibArr[1]
 				fibArr[1] = fibArr[2]
 			}
