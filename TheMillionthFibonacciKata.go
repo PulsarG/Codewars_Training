2f86155638c9eeff9e95eb4cabb79c4a1056936d
PulsarG <pulsar.g.projects@gmail.com>
2024-03-02 13:32:43 +0500

    test#18.9

--- TheMillionthFibonacciKata.go
+++ TheMillionthFibonacciKata.go
@@ -1,19 +1,21 @@
 // The Millionth Fibonacci Kata - 3 kyu
 // https://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/go
-// test#18.8.1
+// test#18.9
 
 package main
 
 import (
 	"fmt"
 	"math/big"
+	"time"
 )
 
 // TODO Сократить, ускорить <12000vc
 
 func main() {
-	n := -9
-	var fibArr [2]big.Int
+	start := time.Now()
+	n := 2000000
+	var fibArr [3]big.Int
 
 	if n >= 0 {
 		for i := 0; i <= int(n); i++ {
@@ -23,9 +25,9 @@ func main() {
 			} else if i == 1 {
 				fibArr[1] = *big.NewInt(int64(i))
 			} else {
-				a := *new(big.Int).Add(&fibArr[0], &fibArr[1])
+				fibArr[2] = *new(big.Int).Add(&fibArr[0], &fibArr[1])
 				fibArr[0] = fibArr[1]
-				fibArr[1] = a
+				fibArr[1] = fibArr[2]
 			}
 		}
 	} else {
@@ -33,11 +35,14 @@ func main() {
 			if i == -1 {
 				fibArr[1] = *big.NewInt(int64(i))
 			} else {
-				a := *new(big.Int).Sub(&fibArr[0], &fibArr[1])
+				fibArr[2] = *new(big.Int).Sub(&fibArr[0], &fibArr[1])
 				fibArr[0] = fibArr[1]
-				fibArr[1] = a
+				fibArr[1] = fibArr[2]
 			}
 		}
 	}
-	fmt.Println(fibArr[1])
+	//fmt.Println(fibArr[1])
+
+	elapsed := time.Since(start)
+	fmt.Println("Время выполнения:", elapsed)
 }
