507471aa6a364d0b50e48702a9eb0132936b8112
PulsarG <pulsar.g.projects@gmail.com>
2024-03-03 14:49:04 +0500

    test#18.9

--- TheMillionthFibonacciKata.go
+++ TheMillionthFibonacciKata.go
@@ -2,7 +2,7 @@
 // https://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/go
 // test#18.9
 
-package main
+/* package main
 
 import (
 	"fmt"
@@ -14,11 +14,11 @@ import (
 
 func main() {
 	start := time.Now()
-	n := 2000000
+	n := 5
 	var fibArr [3]big.Int
 
 	if n >= 0 {
-		for i := 0; i <= int(n); i++ {
+		for i := 0; i <= n; i++ {
 			if i == 0 {
 				fibArr[0] = *big.NewInt(int64(i))
 				fibArr[1] = *big.NewInt(int64(i))
@@ -31,7 +31,7 @@ func main() {
 			}
 		}
 	} else {
-		for i := -1; i >= int(n); i-- {
+		for i := -1; i >= n; i-- {
 			if i == -1 {
 				fibArr[1] = *big.NewInt(int64(i))
 			} else {
@@ -41,7 +41,54 @@ func main() {
 			}
 		}
 	}
-	//fmt.Println(fibArr[1])
+	fmt.Println(fibArr[2])
+
+	elapsed := time.Since(start)
+	fmt.Println("Время выполнения:", elapsed)
+} */
+
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	n := 2000000
+	var fib big.Int
+
+	a, b := big.NewInt(0), big.NewInt(0)
+
+	if n >= 0 {
+		for i := 0; i <= n; i++ {
+			if i == 0 {
+				continue
+			} else if i == 1 {
+				b = big.NewInt(1)
+			} else {
+				fib.Add(a, b)
+				a.Set(b)
+				b.Set(&fib)
+			}
+		}
+	} else {
+		for i := 0; i >= n-1; i-- {
+			if i == 0 {
+				continue
+			} else if i == -1 {
+				b = big.NewInt(-1)
+			} else {
+				fib.Sub(b, a)
+				b.Set(a)
+				a.Set(&fib)
+			}
+		}
+	}
+
+	//fmt.Println(fib)
 
 	elapsed := time.Since(start)
 	fmt.Println("Время выполнения:", elapsed)
